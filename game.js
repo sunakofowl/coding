4aed155363d3ecebeebc0eadcece55e95759f088
ReRe <druen.melanie@gmail.com>
2025-06-05 16:35:54 -0400

    formatted

--- game.js
+++ game.js
@@ -7,29 +7,29 @@ const getChoices = (userInput) => {
     }
 }
 const getComputerChoice = () => {
-   const randomNum = Math.floor(Math.random() * 3) 
-   if(randomNum === 0 )
-    return 'rock'; 
-else if (randomNum === 1)
-    return 'paper';
-else if (randomNum === 2)
-    return 'scissors';
-    }
+    const randomNum = Math.floor(Math.random() * 3)
+    if (randomNum === 0)
+        return 'rock';
+    else if (randomNum === 1)
+        return 'paper';
+    else if (randomNum === 2)
+        return 'scissors';
+}
 
-    const determineWinner = ( UserChoice, computerChoice) => {
-        if (UserChoice === computerChoice) {
-            return "It's a tie!";
-        } else if (
-            (UserChoice === 'rock' && computerChoice === 'scissors') ||
-            (UserChoice === 'paper' && computerChoice === 'rock') ||
-            (UserChoice === 'scissors' && computerChoice === 'paper')
-        ) {
-            return 'You win!';
-        } else {
-            return 'You lose!';
-        }
+const determineWinner = (UserChoice, computerChoice) => {
+    if (UserChoice === computerChoice) {
+        return "It's a tie!";
+    } else if (
+        (UserChoice === 'rock' && computerChoice === 'scissors') ||
+        (UserChoice === 'paper' && computerChoice === 'rock') ||
+        (UserChoice === 'scissors' && computerChoice === 'paper')
+    ) {
+        return 'You win!';
+    } else {
+        return 'You lose!';
     }
+}
 
-    console.log(getChoices('paper'))
-    console.log(getComputerChoice( ))
-    console.log(determineWinner( getChoice, getComputerChoice( )))
+console.log(getChoices('paper'))
+console.log(getComputerChoice())
+console.log(determineWinner(getChoice, getComputerChoice()))
